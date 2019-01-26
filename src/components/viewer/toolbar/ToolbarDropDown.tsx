698e0b9ffd4ea569e7ecabf60c27f7dd4a3b7f9d
MykolaGolubyev <golubev.nikolay@gmail.com>
2019-01-25 19:21:08 -0500

    make toolbar hotkeys work in fullscreen

--- src/components/viewer/toolbar/ToolbarDropDown.tsx
+++ src/components/viewer/toolbar/ToolbarDropDown.tsx
@@ -3,10 +3,6 @@ import * as React from 'react';
 import { ToolbarDropDownItem } from './ToolbarDropDownItem';
 
 import { ToolbarDropDownSelectionPlacement } from './ToolbarDropDownSelectionPlacement';
-import { GlobalHotKeysHandler } from '../../hotkeys/GlobalHotKeysHandler';
-
-import { HotKeyBoundActions } from '../../hotkeys/HotKeyBoundActions';
-
 import { ToolbarDropDownSelectionItem } from './ToolbarDropDownSelectionItem';
 
 import './ToolbarDropDown.css';
@@ -35,24 +31,11 @@ export class ToolbarDropDown extends React.Component<Props, State> {
 
         return (
             <div className="rcv-toolbar-drop-down">
-                <GlobalHotKeysHandler keyBoundActions={this.hotKeyBoundActions()}/>
-
                 <div className="rcv-toolbar-drop-down-label">{label}</div>
                 {this.renderSelected()}
                 {this.renderSelectionIfRequired()}
             </div>
         );
-    }
-
-    private hotKeyBoundActions(): HotKeyBoundActions {
-        const result: HotKeyBoundActions = {};
-
-        const {items} = this.props;
-        items
-            .filter(item => !!item.hotKey)
-            .forEach(item => result[item.hotKey!] = () => this.onItemSelect(item.label));
-
-        return result;
     }
 
     private saveSelectedLabelNode = (node: HTMLDivElement) => {
