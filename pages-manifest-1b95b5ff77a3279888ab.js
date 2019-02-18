a304191fa6f6a3cc4df2398aca41856fb74f956f
MykolaGolubyev <golubev.nikolay@gmail.com>
2019-02-18 18:54:40 -0500

    Updates

---
+++ pages-manifest-1b95b5ff77a3279888ab.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{136:function(a){a.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{"dev-404-page-5f9":"920/path---dev-404-page-5-f-9-fab-NZuapzHg3X9TaN1iIixfv1W23E",index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E"}}}}]);
+//# sourceMappingURL=pages-manifest-1b95b5ff77a3279888ab.js.map
