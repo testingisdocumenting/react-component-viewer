3a238e680eb807c7706e348600c3c5c1e47c87ff
MykolaGolubyev <golubev.nikolay@gmail.com>
2019-02-18 19:00:23 -0500

    minor doc updates

--- documentation/rcv/src/gatsby-config.js
+++ documentation/rcv/src/gatsby-config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    plugins: [`gatsby-plugin-react-helmet`]
+    plugins: [`gatsby-plugin-react-helmet`],
+    pathPrefix: "/react-component-viewer",
 }
--- documentation/rcv/src/pages/index.js
+++ documentation/rcv/src/pages/index.js
@@ -35,28 +35,30 @@ import { profileScreenDemo } from './demos/profileScreen';
 import { formsDemo } from './demos/forms';
 import { sideBySideDemo } from './demos/sideBySide';
 
-const widgets = new Registry('widgets', {componentWrapper: DemoWrapper});
-widgets
+const registries = new Registries();
+registries.add('widgets')
     .registerAsGrid('Links', 300, linksDemo)
     .registerAsTwoColumnTable('Forms', formsDemo)
     .registerAsTwoColumnTable('Buttons', buttonsDemo)
     .registerAsRows('Inputs', inputsDemo);
 
-const layouts = new Registry('layouts');
-layouts
+registries.add('layouts')
     .registerSingle('Side by Side', sideBySideDemo);
 
-const endToEnd = new Registry('end to end');
-endToEnd.registerAsTabs('Long Content', longContentDemo)
+registries.add('end to end')
     .registerSingle('Single Screen', profileScreenDemo)
     .registerAsMiniApp('Single Screen mini app', '/app', profileScreenDemo);
+
+export class App extends React.Component {
+    render() {
+        return (
+            <ComponentViewer registries={registries}/>
+        );
+    }
+}
 \`\`\`    
 
 It will work with your build system, the language of your choice and style processing.
-
-# Installation
-
-    npm install react-component-viewer --save-dev 
 
 `
 }
@@ -79,16 +81,8 @@ export function buttonsDemo(registry: Registry) {
 }
 \`\`\` 
 
-# Registration methods
+# Installation
 
-\`registerAsGrid\` name, minWidth, demoFunction
-
-\`registerAsRows\` name, demoFunction
-
-\`registerAsTwoColumnTable\` name, demoFunction
-
-\`registerAsTabs\` name, demoFunction
-
-\`registerAsSingle\` name, demoFunction
+    npm install react-component-viewer --save-dev 
 `
 }
