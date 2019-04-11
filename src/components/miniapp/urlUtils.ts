d8010444db04e8dbc98d12c05a59bfabe4fda760
MykolaGolubyev <golubev.nikolay@gmail.com>
2019-04-11 08:01:47 -0400

    append current search url when select mini app url to open

--- src/components/miniapp/urlUtils.ts
+++ src/components/miniapp/urlUtils.ts
@@ -5,3 +5,8 @@ export function currentUrl() {
 export function currentUrlMatchesRegexp(urlRegexp: RegExp) {
     return urlRegexp.test(currentUrl());
 }
+
+export function appendUrlSearch(originalUrl: string, search: string) {
+    const hasSearch = originalUrl.indexOf('?') !== -1;
+    return originalUrl + (hasSearch ? '&' : '?') + search.replace('?', '');
+}
